50ffef58d0d8b6982f6fab2539e62eb778871a2f
Süleyman Karakaşoğlu <xtinasfighters@gmail.com>
2021-06-06 05:12:37 +0300

    api url fix

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 
 @Injectable()
 export class Constants {
-  public readonly BASE_API_URL = "https://aybushbot.com:9000";
+  public readonly BASE_API_URL = "https://api.aybushbot.com";
   public readonly API_VER = "v1";
   public readonly EPISODE_ENDPOINT = "episode";
   public readonly LEADERBOARD_ENDPOINT = "leaderboard";
